2a1c94cecfd372caa9dcc870c40d56d26eaab1cc
Delon Borges <delon.borges@icloud.com>
2022-01-29 15:27:13 -0300

    Add database seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into tb_category (name) values ('Accessories');
+insert into tb_category (name) values ('Books');
+insert into tb_category (name) values ('Computers');
+insert into tb_category (name) values ('Electronics');

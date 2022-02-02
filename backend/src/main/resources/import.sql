fe67133f9a1b5b11dd75b8e033b9bcd86f1a8a6b
Delon Borges <delon.borges@icloud.com>
2022-02-01 22:50:28 -0300

    Add product entity

--- backend/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into tb_category (name, created_at) values ('Accessories', NOW());
-insert into tb_category (name, created_at) values ('Books', NOW());
-insert into tb_category (name, created_at) values ('Computers', NOW());
-insert into tb_category (name, created_at) values ('Electronics', NOW());

b350a55218d0ec0504923fd1d863f12f2a832d87
Delon Borges <delon.borges@icloud.com>
2022-02-16 13:52:51 -0300

    Add findAllPaged ProductResourceInterationTest tests

--- backend/src/test/java/com/delonborges/dscatalog/tests/services/ProductServiceIntegrationTests.java
+++ backend/src/test/java/com/delonborges/dscatalog/tests/services/ProductServiceIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.delonborges.dscatalog.tests.services;
 
+import com.delonborges.dscatalog.dto.ProductDTO;
 import com.delonborges.dscatalog.repositories.ProductRepository;
 import com.delonborges.dscatalog.services.ProductService;
 import com.delonborges.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -7,11 +8,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIntegrationTests {
 
     @Autowired
@@ -23,12 +28,55 @@ public class ProductServiceIntegrationTests {
     private Long existingId;
     private Long nonExistingId;
     private Long totalProducts;
+    private Integer firstPage;
+    private Integer totalPages;
+    private Integer validPageSize;
+    private String sortMethod;
+    private PageRequest validPageRequest;
+    private PageRequest invalidPageRequest;
+    private PageRequest sortByNamePageRequest;
 
     @BeforeEach
     protected void setUp() {
         existingId = 1L;
         nonExistingId = 26L;
         totalProducts = 25L;
+        firstPage = 0;
+        totalPages = 3;
+        validPageSize = 10;
+        validPageRequest = PageRequest.of(firstPage, validPageSize);
+        invalidPageRequest = PageRequest.of(totalPages + 1, validPageSize);
+        sortByNamePageRequest = PageRequest.of(firstPage, validPageSize, Sort.by("name"));
+    }
+
+    @Test
+    public void findAllPagedShouldReturnPageWhenPageExists() {
+        Page<ProductDTO> result = productService.findAllPaged(validPageRequest);
+
+        assertFalse(result.isEmpty());
+        assertEquals(firstPage, result.getNumber());
+        assertEquals(totalPages, result.getTotalPages());
+        assertEquals(validPageSize, result.getSize());
+        assertEquals(totalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnEmptyWhenPageDoesNotExist() {
+        Page<ProductDTO> result = productService.findAllPaged(invalidPageRequest);
+
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+        Page<ProductDTO> result = productService.findAllPaged(sortByNamePageRequest);
+
+        String firstItem = "MacBook Pro";
+        String lastItem = "PC Gamer Hera";
+
+        assertFalse(result.isEmpty());
+        assertEquals(firstItem, result.getContent().get(0).getName());
+        assertEquals(lastItem, result.getContent().get(9).getName());
     }
 
     @Test
@@ -43,8 +91,6 @@ public class ProductServiceIntegrationTests {
 
     @Test
     public void deleteShouldThrowExceptionWhenIdDoesNotExist() {
-        assertThrows(ResourceNotFoundException.class, () -> {
-            productService.delete(nonExistingId);
-        });
+        assertThrows(ResourceNotFoundException.class, () -> productService.delete(nonExistingId));
     }
 }

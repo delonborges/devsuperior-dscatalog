525c4e4f38d6a4d9c093b1c3ac1937325e3b150a
Delon Borges <delon.borges@icloud.com>
2022-02-18 22:36:20 -0300

    Add User and Role repositories

---
+++ backend/src/main/java/com/delonborges/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.delonborges.dscatalog.repositories;
+
+import com.delonborges.dscatalog.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}

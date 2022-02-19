6caa9afe3b4edb234d241739ad01b3b826eba9d1
Delon Borges <delon.borges@icloud.com>
2022-02-19 10:02:16 -0300

    Add custom response to beans validation

--- backend/src/main/java/com/delonborges/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/delonborges/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.delonborges.dscatalog.services.exceptions.DatabaseIntegrityViolationE
 import com.delonborges.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -39,6 +41,25 @@ public class ResourceExceptionHandler {
         error.setError("Database integrity violation");
         error.setMessage(exception.getMessage());
         error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> argumentNotValid(MethodArgumentNotValidException exception, HttpServletRequest request) {
+
+        ValidationError error = new ValidationError();
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Argument not valid");
+        error.setMessage(exception.getMessage());
+        error.setPath(request.getRequestURI());
+
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
 
         return ResponseEntity.status(status).body(error);
     }
